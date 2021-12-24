16df579b5550972a16088430e49f394880262718
Nikitaverma2608 <89023534+Nikitaverma2608@users.noreply.github.com>
2021-12-25 00:35:30 +0530

    my task-1

---
+++ Javascript/resource.js
@@ -0,0 +1,41 @@
+//section-1 
+var counter = 1;
+    setInterval(function(){
+      document.getElementById('radio' + counter).checked = true;
+      counter++;
+      if(counter > 4){
+        counter = 1;
+      }
+    }, 5000);
+
+// section-5
+
+function videoslider(links){
+  document.querySelector(".slider").src = links;
+}
+
+// section-7
+window.addEventListener("DOMContentLoaded", function () {
+  // get the form elements defined in your form HTML above
+
+  var form = document.getElementById("my-form");
+  // var button = document.getElementById("my-form-button");
+  var status = document.getElementById("status");
+
+  // Success and Error functions for after the form is submitted
+
+  function success() {
+    form.reset();
+    status.classList.add("success");
+    status.innerHTML = "Thanks!";
+  }
+
+  function error() {
+    status.classList.add("error");
+    status.innerHTML = "Oops! There was a problem.";
+  }  
+
+
+});
+
+
